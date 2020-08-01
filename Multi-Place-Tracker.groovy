12423525bdae4af64c7097a54270412fb8f1ae5b
lnjustin <lnjustin@users.noreply.github.com>
2020-07-31 23:03:08 -0400

    Update Multi-Place-Tracker.groovy

--- Multi-Place-Tracker.groovy
+++ Multi-Place-Tracker.groovy
@@ -1,18 +1,10 @@
- /**
- *  Multi-Place Tracker
+/**
+ * Multi-Place Tracker
  *
- *  Copyright\u00A9 2020 Justin Leonard
+ * Copyright 2020 Justin Leonard
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- * V1.0 - Initial Release
+ * Multi-Place has been licensed to you. By downloading, installing, and/or executing this software you hereby agree to the terms and conditions set forth in the Multi-Place license agreement.
+ * <https://raw.githubusercontent.com/lnjustin/Multi-Place/master/License.md>
 **/
 
 metadata
@@ -23,5 +15,15 @@ metadata
         attribute "tracker", "string"
         attribute "place", "string"
         attribute "vehicle", "string"
+        
+        command "cancelTrip"
     }
+}
+
+def setPersonId(String personId) {
+    state.personId = personId    
+}
+
+def cancelTrip() {
+    parent.cancelCurrentTripForPerson(state.personId)
 }
